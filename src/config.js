9b24924e84e6ad033ea3f5d7bb54b29045082b52
mahesh <maheshmahi61086@gmail.com>
2025-06-27 23:34:33 +0530

    deployed

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const getDevelopmentApiUrl = () => {
 };
 
 const config = {
-  apiUrl: process.env.NODE_ENV === 'production'
+  apiUrl:  process.env.REACT_APP_API_URL || process.env.NODE_ENV === 'production'
     ? 'https://memorymaniaback.onrender.com'
     : getDevelopmentApiUrl()
 };
